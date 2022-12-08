6a988adb4712e748592405beaffbecce718cdd6e
Matheus Macedo <matheusg3.201168@gmail.com>
2022-12-07 22:21:01 -0300

    js initial

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,27 +1,58 @@
 import "./style.sass";
 import Ellipsis from "../../images/icon-ellipsis.svg";
 import Work from "../../images/icon-work.svg";
+import data from "../../../data.json";
+
+// export const Card = () => (
+//   <div className="container-card">
+//       <div className="illustration-top">
+//         <img src={Work} alt="Work" />
+//       </div>
+//       <div className="card">
+//         <div className="type">
+//           <p>Work</p>
+//           <img
+//             src={Ellipsis}
+//             alt="Três
+//             pontinhos"
+//           />
+//         </div>
+//         <div className="info-card">
+//           <p className="d-w-m">32hrs</p>
+//           <p>Last Week - 5hrs</p>
+//         </div>
+//       </div>
+//     </div>
+// )
+
 
 export const Card = () => {
+  const timeframes = data
   return (
     <div className="container-card">
-      <div className="illustration-top">
-        <img src={Work} alt="Work" />
-      </div>
-      <div className="card">
-        <div className="type">
-          <p>Work</p>
-          <img
-            src={Ellipsis}
-            alt="Três 
-        pontinhos"
-          />
-        </div>
-        <div className="info-card">
-          <p className="d-w-m">32hrs</p>
-          <p>Last Week - 5hrs</p>
-        </div>
-      </div>
+      {timeframes.map((time) => {
+        return (
+          <>
+            <div className="illustration-top">
+              <img src={Work} alt="Work" />
+            </div>
+            <div className="card">
+              <div className="type">
+                <p>{time.title}</p>
+                <img
+                  src={Ellipsis}
+                  alt="Três 
+          pontinhos"
+                />
+              </div>
+              <div className="info-card">
+                <p className="d-w-m">32hrs</p>
+                <p>Last Week - 5hrs</p>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 };
--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,11 +1,30 @@
-import { AsideInfo } from "../AsideInfo"
-import { Cards } from "../Cards"
-import './style.sass'
+import data from '../../../data.json'
+import { useEffect, useState } from "react";
+import { AsideInfo } from "../AsideInfo";
+import { Cards } from "../Cards";
+import "./style.sass";
 
-export const MainContent = () =>(
+export const MainContent = () => {
+    // const [works, setWork] = useState({
+    //     title: ''
+    // })
+
+    // const fecthData = () =>{
+    //     data.map(data =>{
+    //     setWork({
+    //         title: data.title
+    //     })
+    // })}
+
+    // useEffect(() =>{
+    //     fecthData()
+    // }, [])
+
+    
+  return (
     <main className="dashboard">
-        <AsideInfo />
-        <Cards />
+      <AsideInfo />
+      <Cards />
     </main>
-)
-
+  );
+};
